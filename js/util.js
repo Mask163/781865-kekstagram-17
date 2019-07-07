a1cc16a7d2a4fadc735d7de1365bbc00c4bf2e56
Mask <vint.max@yandex.ru>
2019-07-07 12:01:31 +0400

    Разбивка на модули

---
+++ js/util.js
@@ -0,0 +1,23 @@
+'use strict';
+
+(function () {
+  var ESC_KEYCODE = 27;
+  var NONE_FILTER = 'effects__preview--none';
+  var CHROME_FILTER = 'effects__preview--chrome';
+  var SEPIA_FILTER = 'effects__preview--sepia';
+  var MARVIN_FILTER = 'effects__preview--marvin';
+  var PHOBOS_FILTER = 'effects__preview--phobos';
+  var HEAT_FILTER = 'effects__preview--heat';
+  var EFFECT_LVL_LINE_WIDTH = 453;
+
+  window.util = {
+    ESC_KEYCODE: ESC_KEYCODE,
+    NONE_FILTER: NONE_FILTER,
+    CHROME_FILTER: CHROME_FILTER,
+    SEPIA_FILTER: SEPIA_FILTER,
+    MARVIN_FILTER: MARVIN_FILTER,
+    PHOBOS_FILTER: PHOBOS_FILTER,
+    HEAT_FILTER: HEAT_FILTER,
+    EFFECT_LVL_LINE_WIDTH: EFFECT_LVL_LINE_WIDTH
+  };
+})();
