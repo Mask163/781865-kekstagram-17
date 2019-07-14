5f23e7bc474ca0b84d2009038c8624ae3e78ece3
Mask <vint.max@yandex.ru>
2019-07-14 16:57:43 +0400

    Загрузка данных с удалённого сервера

--- js/data.js
+++ js/data.js
@@ -1,28 +1,7 @@
 'use strict';
 
 (function () {
-  var photos = [];
-
-  var getRandomArbitrary = function (min, max) { // создание случайного числа
-    return Math.random() * (max - min) + min;
-  };
-
-  var createArray = function () { // создание массива случайных данных
-    var fotoComments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-
-    var fotoNames = ['Антон', 'Алексей', 'Екатерина', 'Зульфия', 'Даздраперма', 'Дмитрий', 'Ольга', 'Евгений', 'Светлана', 'Азам'];
-
-    for (var i = 0; i < 25; i++) {
-      photos[i] = {url: 'photos/' + (i + 1) + '.jpg',
-        likes: Math.round(getRandomArbitrary(15, 200)),
-        comments: fotoComments[Math.round(getRandomArbitrary(0, 5))],
-        name: fotoNames[Math.round(getRandomArbitrary(0, 9))]};
-    }
-  };
-
-  createArray();
-
-  var createFotos = function () { // заполнение блока DOM-элементами на основе массива JS-объектов
+  var createFotos = function (photos) { // заполнение блока DOM-элементами на основе массива JS-объектов
     var photoContainer = document.querySelector('.pictures');
     var photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
@@ -35,5 +14,17 @@
     }
   };
 
-  createFotos();
+  var errorHandler = function (errorMessage) { // сообщение об ошибке
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; padding: 7px; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.load(createFotos, errorHandler);
 })();
